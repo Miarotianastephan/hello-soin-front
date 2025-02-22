01b85473ff79cedacda5e5c31c9fb0467fb231f1
unknown <tokiramanitra21@gmail.com>
2025-02-22 11:39:31 +0100

    commift

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),],
-  
+  host: '0.0.0.0',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
