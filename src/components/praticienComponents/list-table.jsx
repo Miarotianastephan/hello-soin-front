8068611869b24861e614ac52058bfab5e7a9d613
Miarotiana Stephan <miarotianastephan01@gmail.com>
2025-03-10 14:13:47 +0300

    Page Dashboard Praticien
    Page principale du praticien terminé

--- src/components/praticienComponents/list-table.jsx
+++ src/components/praticienComponents/list-table.jsx
@@ -15,9 +15,11 @@ import {
   Avatar,
   IconButton,
   Tooltip,
+  Drawer
 } from "@material-tailwind/react";
 import { format } from "date-fns";
 import { fr } from "date-fns/locale";
+import { Info, MailIcon, MapPin, Phone } from "lucide-react";
 import React, { useEffect, useState } from "react";
  
 const TABS = [
@@ -84,9 +86,115 @@ const TABLE_ROWS = [
     date: "04/10/21",
   },
 ];
+
+const DetailsPanel = ({isOpened, closePanel, selectedAppoint}) => {
+
+  return(
+    <>
+      <Drawer placement="right" overlay={false} open={isOpened} onClose={closePanel} className="p-4 shadow">
+        {/* Information principale */}
+        <div className="mb-6 flex flex-col items-end gap-2">
+          { selectedAppoint === null ? (
+                    <p className="flex items-center gap-2 text-gray-600">
+                      <Info className="w-5 h-5 text-helloSoin" />
+                      Aucune détails à afficher
+                    </p>
+          ) : (<>
+              <IconButton variant="text" color="blue-gray" onClick={closePanel} >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </IconButton>
+              <div className="flex w-full gap-3 items-start justify-between">
+                <img
+                  src="https://i.pravatar.cc/300"
+                  alt="Aperçu de la photo de profil"
+                  className="h-20 w-20 rounded-lg object-cover"
+                />
+                <div>
+                  <Typography variant="h5" className="mt-1 font-bold text-helloBlue">
+                  {selectedAppoint ? `${selectedAppoint.patient.nom} ${selectedAppoint.patient.prenom}` : ""}
+                  </Typography>
+                  <Typography
+                    variant="small"
+                    className="flex gap-1 mt-1 text-helloBlue break-all"
+                  >
+                    <MailIcon className="h-4 w-4" />
+                    {selectedAppoint ? selectedAppoint.patient.email : ""}
+                  </Typography>
+                  <Typography
+                    variant="small"
+                    className="flex gap-1 mt-1 text-helloBlue"
+                  >
+                    <Phone className="h-4 w-4" />
+                    {selectedAppoint ? selectedAppoint.patient.numero : ""}
+                  </Typography>
+                  {/* <Typography
+                    variant="small"
+                    className="flex gap-4 mt-1 text-helloBlue"
+                  >
+                    <MapPin className="h-4 w-4" />
+                    {selectedAppoint ? selectedAppoint.patient.lieu : ""}
+                  </Typography> */}
+                </div>
+              </div>
+            </>)}
+        </div>
+        {/* Detail des rendez-vous */}
+        <div className="">
+          <Typography variant="h6" className="font-bold text-helloBlue">
+            Détails du rendez-vous 
+          </Typography>
+          <ul className="space-y-2 text-helloBlue">
+            <li className="flex">
+              <span className="flex-1">Date</span>
+              <span className="flex-1 text-start">{selectedAppoint ? selectedAppoint.date : ""}</span>
+            </li>
+            <li className="flex">
+              <span className="flex-1">Type de pratique</span>
+              <span className="flex-1 text-start">{selectedAppoint ? selectedAppoint.practice.type : ""}</span>
+            </li>
+            <li className="flex">
+              <span className="flex-1">Heure de début</span>
+              <span className="flex-1 text-start">{selectedAppoint ? selectedAppoint.practice.start : ""}</span>
+            </li>
+            <li className="flex">
+              <span className="flex-1">Heure de fin</span>
+              <span className="flex-1 text-start">{selectedAppoint ? selectedAppoint.practice.end : ""}</span>
+            </li>
+            <li className="flex">
+              <span className="flex-1">Tarif (en euro)</span>
+              <span className="flex-1 text-start">---</span>
+            </li>
+            <li className="flex flex-col">
+              <span className="flex-1">Déscription :</span>
+              <span className="flex-1 text-start">{selectedAppoint ? selectedAppoint.motif : ""}</span>
+            </li>
+          </ul>
+        </div>
+      </Drawer>
+    </>
+  );
+}
  
 export function ListTable({todayAppointments}) {
   const [dataRDV, setDataRDV] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [selectedAppoint, setSelectedAppoint] = useState(null);
+ 
+  const openDrawer = () => setOpen(true);
+  const closeDrawer = () => setOpen(false);
 
   useEffect(() => {
     setDataRDV(todayAppointments);
@@ -124,8 +232,15 @@ export function ListTable({todayAppointments}) {
     setDataRDV(resultSearch);
   }
 
+  function handleShowDetailPanel(details){
+    setSelectedAppoint(details)
+    openDrawer();
+    console.log(details)
+  }
+
   return (
     <Card >
+      <DetailsPanel isOpened={open} closePanel={closeDrawer} selectedAppoint={selectedAppoint} />
       <CardHeader floated={false} shadow={false} className="p-4 rounded-none">
         <div className=" flex items-center justify-between">
           <div>
@@ -247,7 +362,7 @@ export function ListTable({todayAppointments}) {
                     </td>
                     <td className={classes}>
                       <Tooltip content="Voir plus">
-                        <IconButton variant="text">
+                        <IconButton variant="text" onClick={() => handleShowDetailPanel(dataRDV[index])}>
                           <ArrowRightCircleIcon className="h-6 w-6 text-helloBlue" />
                         </IconButton>
                       </Tooltip>
