6a0b165815682eb53422fc1628e0ef00c114f33c
Miarotiana Stephan <miarotianastephan01@gmail.com>
2025-03-12 15:53:18 +0300

    12 mars 2025
    - Liste des rendez-vous modifier
    - Mise en place de Poppins comme font principale

--- src/pages/disponibilite/agenda/tableComponents/AppointmentsList.jsx
+++ src/pages/disponibilite/agenda/tableComponents/AppointmentsList.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { getColorByType } from '../utils/agendaUtils';
+import { IconButton, Tooltip, Typography } from '@material-tailwind/react';
+import { Info } from 'lucide-react';
 
 const AppointmentsList = ({ appointments }) => (
-  <div className="p-1">
-    <div className="flex gap-4 my-2">
+  <div className="p-1 text-helloBlue">
+    {/* Legende por les types de pratiques */}
+    <div className="flex gap-8 mb-2 p-2">
       <div className="flex items-center">
         <span
           className="mr-1"
@@ -44,25 +47,29 @@ const AppointmentsList = ({ appointments }) => (
         <span>Hypnose</span>
       </div>
     </div>
+    
     {appointments.length === 0 ? (
       <p>Aucun rendez‑vous.</p>
     ) : (
+      //  border-separate border-spacing-4
       <table className="w-full">
         <thead>
-          <tr className="text-left border-b bg-gray-200">
-            <th></th>
-            <th>Date</th>
-            <th>Heure</th>
-            <th>Patient</th>
-            <th>Motif</th>
+          <tr className="text-left">
+            <th className="rounded-l-full p-4"></th>
+            <th className="p-4">Nom</th>
+            <th className="p-4">Telephone</th>
+            <th className="p-4">Date</th>
+            <th className="p-4">Pratique</th>
+            <th className="p-2">Heure</th>
+            <th className="rounded-r-full p-4">Motif</th>
           </tr>
         </thead>
         <tbody>
           {appointments
             .sort((a, b) => new Date(b.date) - new Date(a.date))
             .map((app, idx) => (
-              <tr key={idx} className="border-b">
-                <td>
+              <tr key={idx} className="odd:bg-gray-100 gap-4">
+                <td className="rounded-l-full px-4">
                   <span
                     style={{
                       width: '10px',
@@ -73,13 +80,59 @@ const AppointmentsList = ({ appointments }) => (
                     }}
                   />
                 </td>
-                <td>{app.date}</td>
-                <td>
+                <td className="px-4">
+                  <div className="flex items-center gap-3">
+                    {/* <Avatar src={img} alt={name} size="sm" /> */}
+                    <div className="flex flex-col">
+                      <Typography
+                        variant="small"
+                        className="font-bold text-helloBlue"
+                      >
+                        {app.patient.nom} {app.patient.prenom}
+                      </Typography>
+                      <Typography
+                        variant="small"
+                        className="font-normal opacity-70 text-helloBlue"
+                      >
+                        {app.patient.email}
+                      </Typography>
+                    </div>
+                  </div>
+                </td>
+                <td className="px-4">
+                  <Typography
+                    variant="small"
+                    className="w-max font-normal opacity-70 text-helloBlue"
+                  >
+                    {app.patient.numero}
+                  </Typography>
+                </td>
+                <td className="px-4">
+                  <Typography
+                    variant="small"
+                    className="w-max font-normal opacity-70 text-helloBlue"
+                  >
+                    {app.date}
+                  </Typography>
+                </td>
+                <td className="px-4">{app.practice.type}</td>
+                <td className="w-max px-2">
+                  <Typography
+                    variant="small"
+                    className="font-normal opacity-70 text-helloBlue whitespace-nowrap overflow-hidden text-ellipsis max-w-[150px]"
+                  >
                   {app.practice.start} - {app.practice.end}
+                  </Typography>
                 </td>
-                <td>
+                <td className="rounded-r-full px-4">
+                  <Tooltip content={app.motif || "Motif absent !"}>
+                    <IconButton
+                      variant="text"
+                    >
+                      <Info className="h-5 w-5" />
+                    </IconButton>
+                  </Tooltip>
                 </td>
-                <td>{app.motif}</td>
               </tr>
             ))}
         </tbody>
